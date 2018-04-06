25b001d73bac64cf1b1cf0bba7e520d505c86a5f
wenoptk <wenoptics@163.com>
2018-04-07 00:32:49 +0800

    clean code

--- ClassicLeetcode.js
+++ ClassicLeetcode.js
@@ -11,68 +11,46 @@
 // ==/UserScript==
 
 (function () {
-  
-    var cfg = {
-        "use_wait": false
-    }
-    
-    var observeDOM = (function(){
-      var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-          eventListenerSupported = window.addEventListener;
-  
-      return function(obj, callback){
-        if( MutationObserver ){
-          // define a new observer
-          var obs = new MutationObserver(function(mutations, observer){
-            if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-              callback();
-          });
-          // have the observer observe foo for changes in children
-          obs.observe( obj, { childList:true, subtree:true });
-        }
-        else if( eventListenerSupported ){
-          obj.addEventListener('DOMNodeInserted', callback, false);
-          obj.addEventListener('DOMNodeRemoved', callback, false);
-        }
-      };
-    })();
-    
-    function waitForElement(elementQuery, callBack){
-      window.setTimeout(function(){
-        var element = document.querySelector(elementQuery);
-        if(element){
-          callBack(elementQuery, element);
-          // Continue to find DOM changes
-          waitForElement(elementQuery, callBack);
-        }else{
-          waitForElement(elementQuery, callBack);
-        }
-      },500)
-    }
-    
-    function setStyle() {
-      var e_list_tr = document.querySelectorAll("div.table-responsive.question-list-table .reactable-data tr")
-      for (var i=0; i<e_list_tr.length; i++) {
-        var e_id = e_list_tr[i].querySelectorAll("td")[1];
-        var int_id = parseInt(e_id.innerHTML);
-        if (int_id && int_id <= 300) {
-            e_id.style.backgroundColor = "#ffffd7";
-        }
+
+  var cfg = {}
+
+  var observeDOM = (function () {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+      eventListenerSupported = window.addEventListener;
+
+    return function (obj, callback) {
+      if (MutationObserver) {
+        // define a new observer
+        var obs = new MutationObserver(function (mutations, observer) {
+          if (mutations[0].addedNodes.length || mutations[0].removedNodes.length)
+            callback();
+        });
+        // have the observer observe foo for changes in children
+        obs.observe(obj, { childList: true, subtree: true });
       }
-      console.log("len=="+e_list_tr.length);
+      else if (eventListenerSupported) {
+        obj.addEventListener('DOMNodeInserted', callback, false);
+        obj.addEventListener('DOMNodeRemoved', callback, false);
+      }
+    };
+  })();
+
+  function setStyle() {
+    var e_list_tr = document.querySelectorAll("div.table-responsive.question-list-table .reactable-data tr")
+    for (var i = 0; i < e_list_tr.length; i++) {
+      var e_id = e_list_tr[i].querySelectorAll("td")[1];
+      var int_id = parseInt(e_id.innerHTML);
+      if (int_id && int_id <= 300) {
+        e_id.style.backgroundColor = "#ffffd7";
+      }
     }
-    
-    if (cfg.use_wait) {
-      // This is deprecated.
-      waitForElement("div.table-responsive.question-list-table .reactable-data tr", function(_, __){
-              setStyle();
-        }); 
-    } else {
-      console.log("Start observing: "+document.getElementById("question-app"));
-      observeDOM(document.getElementById("question-app"), function() {
-        console.log("changed: " + document.getElementById("question-app"));
-            setStyle();
-      });
-    }
-   
-  }());
+    console.log("problem list len: " + e_list_tr.length);
+  }
+
+  console.log("Start observing: " + document.getElementById("question-app"));
+  observeDOM(document.getElementById("question-app"), function () {
+    console.log("changed: " + document.getElementById("question-app"));
+    setStyle();
+  });
+
+}());
